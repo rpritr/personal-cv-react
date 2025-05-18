fee538a5e053fd68bb695d6cf6a9604bf761865d
rpritr <robi@foreach-labs.si>
2025-05-18 13:54:32 +0200

    minor fixes

--- src/js/Components/Section/Section.js
+++ src/js/Components/Section/Section.js
@@ -4,7 +4,7 @@ import "./_section.scss";
 
 const Section = (props) => {
     return (
-        <section className={"section " + props.class} >
+        <section className={"section " + props.class} id={props.id}>
             <h2 className="section__title">{props.heading}</h2>
             <p className="section__subtitle">{props.subtitle}</p>
             {props.children}
