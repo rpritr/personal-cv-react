ce32dc73c5ddb61275e79f655ba1bc94550c512b
rpritr <robi@foreach-labs.si>
2025-05-18 09:56:17 +0200

    project setup

---
+++ src/js/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Components/Header/Header";
+
+const Home = () => {
+    return (
+        <>
+            <Header/>
+            <p>Home</p>
+        </>
+
+    )
+}
+
+export default Home;
